3725249176e280ebfe57c0208a8710dca1f663f2
Sainath Machunuru <machunursainathgoud@gmail.com>
2022-06-09 00:48:29 +0530

    Enabled catalog service as eureka client

--- catalogservice/src/main/java/com/msng/catalogservice/CatalogserviceApplication.java
+++ catalogservice/src/main/java/com/msng/catalogservice/CatalogserviceApplication.java
@@ -2,7 +2,9 @@ package com.msng.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CatalogserviceApplication {
 
