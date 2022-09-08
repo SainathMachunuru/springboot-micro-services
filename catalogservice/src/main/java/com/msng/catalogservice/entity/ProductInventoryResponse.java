03e95b1c1760bad54d8b3117e9069ae1a2dbbbd5
Sainath Machunuru <machunursainathgoud@gmail.com>
2022-09-08 14:54:57 +0530

    Call inventory service from catalog service using service discovery and configured hystrix for fallbacks

--- catalogservice/src/main/java/com/msng/catalogservice/entity/ProductInventoryResponse.java
+++ catalogservice/src/main/java/com/msng/catalogservice/entity/ProductInventoryResponse.java
@@ -1,4 +1,31 @@
 package com.msng.catalogservice.entity;
 
 public class ProductInventoryResponse {
+    private int id;
+    private String productCode;
+    private Integer availableQuantity = 0;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public Integer getAvailableQuantity() {
+        return availableQuantity;
+    }
+
+    public void setAvailableQuantity(Integer availableQuantity) {
+        this.availableQuantity = availableQuantity;
+    }
 }
--- catalogservice/src/main/java/com/msng/catalogservice/service/ProductService.java
+++ catalogservice/src/main/java/com/msng/catalogservice/service/ProductService.java
@@ -1,11 +1,17 @@
 package com.msng.catalogservice.service;
 
 import com.msng.catalogservice.entity.Product;
+import com.msng.catalogservice.entity.ProductInventoryResponse;
 import com.msng.catalogservice.repository.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+import com.msng.catalogservice.service.InventoryServiceClient;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -14,10 +20,12 @@ import java.util.Optional;
 @Slf4j
 public class ProductService {
     private final ProductRepository productRepository;
+    private final InventoryServiceClient inventoryServiceClient;
 
     @Autowired
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, InventoryServiceClient inventoryServiceClient) {
         this.productRepository = productRepository;
+        this.inventoryServiceClient = inventoryServiceClient;
     }
 
     public List<Product> findAllProducts() {
@@ -25,6 +33,24 @@ public class ProductService {
     }
 
     public Optional<Product> findProductByCode(String code) {
-        return productRepository.findByCode(code);
+        Optional<Product> productOptional = productRepository.findByCode(code);
+        if(productOptional.isPresent()) {
+            log.info("Fetching inventory level for product_code: "+code);
+            Optional<ProductInventoryResponse> productInventoryResponse  = inventoryServiceClient.getProductInventoryByCode(code);
+            log.info("Inventory of given product: ", productInventoryResponse.toString());
+//            ResponseEntity<ProductInventoryResponse> itemResponseEntity =
+//                    restTemplate.getForEntity("http://inventory-service/api/inventory/{code}",
+//                            ProductInventoryResponse.class,
+//                            code);
+//            if(itemResponseEntity.getStatusCode() == HttpStatus.OK) {
+//                Integer quantity = itemResponseEntity.getBody().getAvailableQuantity();
+//                log.info("Available quantity: "+quantity);
+////                productOptional.get().setInStock(quantity> 0);
+//            } else {
+//                log.error("Unable to get inventory level for product_code: "+code +
+//                        ", StatusCode: "+itemResponseEntity.getStatusCode());
+//            }
+        }
+        return productOptional;
     }
 }
