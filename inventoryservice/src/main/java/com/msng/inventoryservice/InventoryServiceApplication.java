ad23ba9f663fd977a62b8f739c58387eca064d03
Sainath Machunuru <machunursainathgoud@gmail.com>
2022-09-09 00:10:00 +0530

    Added spring cloud sleuth and zipkin for distributed tracing

--- inventoryservice/src/main/java/com/msng/inventoryservice/InventoryServiceApplication.java
+++ inventoryservice/src/main/java/com/msng/inventoryservice/InventoryServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 
 public class InventoryServiceApplication {
